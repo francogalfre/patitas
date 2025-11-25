2f8e8d77b577d54e9cfe0c07086ba7153e903426
francogalfre <francogalfre.code@gmail.com>
2025-11-25 01:14:15 -0300

    feat(auth): :goal_net: handle 'user already exists' error with custom message

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -4,20 +4,20 @@ import AuthWrapper from "../components/wrapper";
 import AuthRegisterForm from "./components/form";
 
 export const metadata: Metadata = {
-	title: "Únete a la comunidad | Patitas",
-	description:
-		"Regístrate en Patitas y únete a nuestra comunidad para ayudar a los animales que buscan un hogar.",
+  title: "Únete a la comunidad | Patitas",
+  description:
+    "Regístrate en Patitas y únete a nuestra comunidad para ayudar a los animales que buscan un hogar.",
 };
 
 const PatitasRegisterPage = () => {
-	return (
-		<AuthWrapper
-			image="/auth/register.webp"
-			alt="Un niño abrazando a su perro de raza Golden, que está acostado en el césped"
-		>
-			<AuthRegisterForm />
-		</AuthWrapper>
-	);
+  return (
+    <AuthWrapper
+      image="/auth/register.webp"
+      alt="Un niño abrazando a su perro de raza Golden, que está acostado en el césped"
+    >
+      <AuthRegisterForm />
+    </AuthWrapper>
+  );
 };
 
 export default PatitasRegisterPage;
